b46be4ee5b345849216b01b58806ddc5cf7f5029
ludo <ludovic.fardel@gmail.com>
2020-03-10 21:49:28 +0100

    Hello LudoPom

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div
+    style={{
+      textAlign: "center",
+      fontFamily: "helvetica",
+      fontSize: 20,
+      textShadow: "pink 2px  2px 10px",
+    }}
+  >
+    Welcome to LudoPom!
+  </div>
+)
